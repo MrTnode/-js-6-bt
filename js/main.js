0661b98d2205b57746da4398d00cf0c141c180b7
ezedward <trung.nguyen280901@hcmu.edu.vn>
2021-06-12 18:23:07 +0700

    BC12_BaiTap_JS_Buoi_6.pdf_nguyendinhtrung

---
+++ js/main.js
@@ -0,0 +1,48 @@
+//Tìm số nguyên dương nhỏ nhất sao cho 1 + 2 + n < 1000
+var btnAccess = document.getElementById('btnAccess')
+btnAccess.addEventListener('click',function(){
+    var pKetQua = document.getElementById('pKetQua')
+    var sum = 0;
+    for ( var i = 0 ; i < 200; i++){
+        sum += i;
+        if (sum > 1000){
+            pKetQua.innerHTML = i*1-1;    
+            break;
+        }
+    }
+})
+//Viết chương trình nhập vào 2 số x, n tính tổng: S(n) = x**1 + x**2 + x**3 +...+x**n(sử dụng vòng lặp và hàm)
+var getEle = function(id){
+    return document.getElementById(id);
+}
+
+var btnSubmit = getEle('btnSubmit');
+btnSubmit.onclick= function(){
+    var x = getEle('ipX').value;
+    var n = getEle('ipN').value;
+    sum = 0
+    for (var i =1; i <= n; i++){
+        sum += x**i;
+    }
+    pTong.innerHTML = sum;
+}
+
+//Nhập vào n. Tính giai thừa 1*2*...*n
+var btnTinhGiaiThua = getEle('btnTinhGiaiThua');
+btnTinhGiaiThua.onclick = function(){
+    var pGiaiThua = getEle('pGiaiThua');
+    var n2 = getEle('ipN2').value;
+    var giaiThua = 1;
+    for (var i = 1; i <= n2; i++){
+        giaiThua = giaiThua*i;
+    }
+    pGiaiThua.innerHTML = 'Giai Thừa của ' + n2 + ' là: ' + giaiThua;
+}
+
+//Hãy viết chương trình khi click vào button sẽ in ra 10 thẻ div. Nếu div nào vị trí chẵn thì background màu đỏ và lẻ thì background màu xanh
+
+var btnTaoDiv = getEle('btnTaoDiv');
+var divBackground = getEle('divBackground');
+btnTaoDiv.onclick = function(){
+    divBackground.style.display = 'block'
+}
